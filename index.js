f83c3218053d7ddc8e6f0e8723a5e990c172e44e
Royce Chua <royce236@gmail.com>
2020-12-02 23:11:44 +0800

    Add POST Message

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const express = require('express')
 const app = express()
 
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded())
+
 let data = [
     { id: 1, message: "Hello World" },
 ]
 
-let errorMessage = {
-    message: "Record not found"
-}
+let successMessage = (message, data) => ({
+    message,
+    data
+}) 
 
+let errorMessage = (message) => ({
+    message
+})
+
+// Fetch all
 app.get('/', (req, res) => res.send(data))
 
+// Search for a specific message
 app.get('/search/', (req, res) => {
     let result = data.filter(d => d.id == req.query.id);
     if (result.length > 0) {
-        return res.status(200).send(result);
+        return res.status(200).json(result);
     }
-    return res.status(404).send(errorMessage);
+    return res.status(404).json(errorMessage("Record not found"));
 })
+
+// Add a message object to the list
+app.post('/message', (req, res) => {
+    if(req.body.data && req.body.data.id && req.body.data.message) {
+        data.push(req.body.data)
+        return res.status(200).json(successMessage("Add message successful", req.body.data))
+    } else {
+        return res.status(422).send(errorMessage('Unprocessable Entity'))
+    }
+})
+
 
 app.listen(3000, () => console.log('Server ready'))
