488a841178f350c4b7e2524f763648d02547621a
Royce Chua <royce236@gmail.com>
2020-12-02 23:32:13 +0800

    add delete

--- index.js
+++ index.js
@@ -35,10 +35,24 @@ app.post('/message', (req, res) => {
     if(req.body.data && req.body.data.id && req.body.data.message) {
         data.push(req.body.data)
         return res.status(200).json(successMessage("Add message successful", req.body.data))
-    } else {
-        return res.status(422).send(errorMessage('Unprocessable Entity'))
-    }
+    } 
+    
+    return res.status(422).send(errorMessage('Unprocessable Entity'))
 })
 
+// delete a message object to the list
+app.delete('/message/:id', (req, res) => {
+    if(req.params.id) {
+        let new_data = data.filter(d => d.id != req.params.id)
+        if(new_data.length != data.length) {
+            data = new_data;
+            return res.status(200).send(successMessage('Delete message success'))
+        } 
+
+        return res.status(404).send(errorMessage('Message record was not found'))
+    } 
+    
+    return res.status(422).send(errorMessage('Unprocessable Entity'))
+})
 
 app.listen(3000, () => console.log('Server ready'))
